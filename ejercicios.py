6d57224f763319d411ba6d9eb66966972e2c589f
mloera <miguelangel.loera@stud.uni-goettingen.de>
2015-11-14 00:03:17 -0600

    Create ejercicios.py

---
+++ ejercicios.py
@@ -0,0 +1,12 @@
+#### No.1 Traducir una secuencia de DNA
+####
+#### Crea una función que divida una secuencia de DNA en sus codones
+#### correspondientes y luego crea otra función que permita traducir
+#### dicha secuencia en una proteína. Utiliza la siguiente
+#### tabla de codones como referencia.
+
+
+#### No.2 Extrae la información referente al Distrito Federal, Guadalajara y
+#### Monterrey de las bases de datos del INEGI (están en tu bandeja del correo
+#### electrónico) y genera un archivo .csv nuevo que contenga ese subconjunto
+#### de datos.
