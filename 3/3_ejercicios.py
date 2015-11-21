deb846352a34873f2b5f286e6b921ca5b435d9b5
mloera <miguelangel.loera@stud.uni-goettingen.de>
2015-11-21 00:03:16 -0600

    Create 3_ejercicios.py

---
+++ 3/3_ejercicios.py
@@ -0,0 +1,16 @@
+## EJERCICIOS
+
+## No. 1
+## ¿Recuerdas los scripts que utilizamos en la sesión 2 para abrir
+## múltiples archivos y filtrarlos?
+## Pues bien, en este ejercicio deberás crear una clase que contenga
+## las funciones necesarias para hacer esas acciones.
+
+## No. 2
+## Prueba que tu clase funciona al importarla en un script nuevo.
+
+## No. 3
+## Crea un script para descargar todos los archivos pdf del siguiente
+## url: http://www.fcb.uanl.mx/nw/es/oferta/planes-de-estudios/plan-de-estudios-401-biologo
+## OJO: salva a los archivos con su nombre original.
+
