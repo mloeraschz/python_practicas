959e99703b4e68b0c908c18fabeed4767560c1a8
mloera <miguelangel.loera@stud.uni-goettingen.de>
2015-11-28 07:42:45 -0600

    Create Ejercicios.py

---
+++ 4/Ejercicios.py
@@ -0,0 +1,5 @@
+## 1. Haz una interfaz gráfica que te permita abrir y leer un archivo de los del INEGI (segunda sesión)
+## 2. Crea un Entry en tu GUI que tenga un label que diga "Estado"
+## 3. Crea un botón en tu GUI que busque el valor de tu entry "Estado" en tu archivo del INEGI seleccioando. Haz que el botón
+##    imprima los renglones de tu archivo INEGI donde aparezca el valor del entry.
+## 4. Crea una GUI en la que puedas escribir una URL y puedas bajar sus imágenes haciendo clic en un botón.
