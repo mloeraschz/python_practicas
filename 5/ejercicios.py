efe4a6c214416babad8691520f87698649fdaa82
mloera <miguelangel.loera@stud.uni-goettingen.de>
2015-12-05 07:53:30 -0600

    Create ejercicios.py

---
+++ 5/ejercicios.py
@@ -0,0 +1,16 @@
+## EJERCICIOS DE LA SESIÓN 5
+##
+## No. 1.- Abre el archivo ARCH655.CSV
+## No. 2.- Crea dos funciones: una para encontrar el promedio de una listas
+## y otra para encontrar la desvicación estándar
+## No. 3.- Encuentra el promedio de la población total de los municipios
+## del archivo ARCH655.CSV. Encuentra el promedio para hombres y mujeres.
+## No. 4.- Encuentra los municipios con la mayor y menor proporción
+## hombres/mujeres
+## No. 5.- Accede a la página https://es.wikipedia.org/wiki/Anexo:Municipios_de_Nuevo_Le%C3%B3n
+## utilizando urllib y urllib2, e interpreta el html usando BeautifulSoup
+## No. 6.- Crea una lista con las urls que dirigen al artículo específico
+## de cada municipio en la página anterior
+## No. 7.- Accede a cada página de la lista anterior, interpreta su html
+## y extrae el nombre del alcalde de cada item. Crea excepciones
+## para los articulos que no contengan esta información.
